f3a4cac98099a27536c4904349a0e1270b165b97
phanit1 <babithota5@msitprogram.net>
2019-11-10 02:44:47 +0530

    ALL DONE

--- Module 9/Ransom Note/RansomNote.java
+++ Module 9/Ransom Note/RansomNote.java
@@ -12,8 +12,8 @@ class RansomNoteFunctions {
         this.hashTable = new SeparateChainingHashST<String,Integer>();
     }
     /**
-     * This is to add the given word 
-     * @param word , the word to be added 
+     * This is to add the given word to hashTable 
+     * @param word , the word to be added in HashTable
      */
     public void add(String word) {
         if (hashTable.contains(word)) {
@@ -23,6 +23,11 @@ class RansomNoteFunctions {
             hashTable.put(word,1);
         }
     }
+    /**
+     * To check whether ransom note can be made from magazine or not
+     * @param word , the word which is checked
+     * @return true if ransom note can be made from magazine, false otherwise
+     */
     public boolean checkAndUpdate(String word) {
         if (hashTable.contains(word)) {
             hashTable.put(word, hashTable.get(word) - 1);
@@ -37,14 +42,16 @@ class RansomNoteFunctions {
 public class RansomNote{
 public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    // creating object to use ransomnote functions
     RansomNoteFunctions hashTable = new RansomNoteFunctions();
-
     String[] magazine = scan.nextLine().split(" ");
+    // add each word in magazine into hashtable
     for (String eachWord : magazine) {
         hashTable.add(eachWord);
     }
     String[] noteWords = scan.nextLine().split(" ");
     boolean canMakeNote = true;
+    // check every each word in notewords and goes inside
     for (String eachWord : noteWords) {
         if (! hashTable.checkAndUpdate(eachWord)) {
             canMakeNote = false;
